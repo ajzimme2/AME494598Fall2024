a8cb59787e542834e33c64ad617072b8f98cca81
ajzimme2 <ajzimme2@asu.edu>
2024-12-01 12:54:43 -0700

    First attempt after receiving the quatro, hopefully it goes well

---
+++ Final_Project/README.md
@@ -0,0 +1,2 @@
+# Beginning of README File
+Instead of using the orginially intended devices I listed in my BOM, I have been provided a 'Quatro' by the professor for input data. 
--- Final_Project/server/server.js
+++ Final_Project/server/server.js
@@ -12,105 +12,89 @@ var VALUEtime = 0;
 let MongoClient = require('mongodb').MongoClient;
 const connectionString = 'mongodb://localhost:27017';
 
-app.get("/", function (req, res) {
-    res.redirect("/index.html");
-});
-
-app.get("/getAverage", function (req, res) {
-  var from = parseInt(req.query.from);
-  var to = parseInt(req.query.to);
-  db.collection("dataWeather").find({time:{$gt:from, $lt:to}}).toArray(function(err, result){
-    console.log(err);
-    console.log(result);
-    var tempSum = 0;
-    var humSum = 0;
-    for(var i=0; i< result.length; i++){
-      tempSum += result[i].t || 0;
-      humSum += result[i].t || 0;
-    }
-    var tAvg = tempSum/result.length;
-    var hAvg = humSum/result.length;
-    res.send(tAvg + " "+  hAvg);
-  });
-});
-
-app.get("/getLatest", function (req, res) {
-  (async function() {
-    let client = await MongoClient.connect(connectionString, { useNewUrlParser: true });
-    let db = client.db('sensorData');
-    try {
-      let result = await db.collection("data").find().sort({time:-1}).limit(10).toArray();
-      res.send(JSON.stringify(result));
-    }
-    finally {
-      client.close();
-    }
-  })().catch(err => console.error(err));
-});
-
-app.get("/getData", function (req, res) {
-  var from = parseInt(req.query.start);
-  var duration = parseInt(req.query.duration);
-  var to = from + duration * 60 * 1000;
-  console.log(`Fetching data from ${from} to ${to}`);  // Log start and end time for debugging
-  // Fetch data from MongoDB
-  
-  (async function() {
-      let client = await MongoClient.connect(connectionString, { useNewUrlParser: true });
-      let db = client.db('sensorData');
-      try {
-          let result = await db.collection("data").find({ time: { $gt: from, $lt: to } }).sort({ time: -1 }).toArray();
-  
-          if (result.length < (duration * 60)) {
-              res.status(400).send("Not enough data available for the requested duration.");
-              return;
-          }
-  
-          res.send(JSON.stringify(result));
-      } finally {
-          client.close();
-      }
-  })().catch(err => {
-      console.error(err);
-      res.status(500).send("Internal server error");
-  });  
-});
-
-
-app.get("/getValue", function (req, res) {
-  res.send(VALUEt.toString() + " " + VALUEh + " " + VALUEtime + "\r");
-});
-
-app.get("/setValue", function (req, res) {
-  VALUEt = parseFloat(req.query.t);
-  VALUEh = parseFloat(req.query.h);
-  VALUEtime = new Date().getTime();
-  var dataObj = {
-    t: VALUEt,
-    h: VALUEh,
-    time: VALUEtime
-  }
-  res.send(VALUEtime.toString());
-  (async function() {
-    let client = await MongoClient.connect(connectionString, { useNewUrlParser: true });
-    let db = client.db('sensorData');
-    try {
-      result = await db.collection("data").insertOne(dataObj);
-      if(result.insertedId) {
-        result = result.insertedId.toString();
-        console.log(result);
-      }
-    }
-    finally {
-      client.close();
-    }
-  })().catch(err => console.error(err));
-});
+const WebSocket = require('ws'); // WebSocket library
 
 app.use(methodOverride());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler());
 
-console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+// Serve static files (e.g., index.html)
+app.get("/", function (req, res) {
+    res.redirect("/index.html");
+});
+
+// Initialize WebSocket server
+const server = app.listen(port, () => {
+    console.log("Server running at http://" + hostname + ":" + port);
+});
+
+const wss = new WebSocket.Server({ server });
+
+// Simulate sending real-time data (replace this with actual data logic)
+wss.on('connection', (ws) => {
+    console.log("WebSocket connection established");
+
+    // Send periodic updates
+    const interval = setInterval(() => {
+        const movementData = {
+            w: Math.random(),
+            x: Math.random() * 10 - 5,
+            y: Math.random() * 10 - 5,
+            z: Math.random() * 10 - 5
+        };
+        ws.send(JSON.stringify(movementData));
+    }, 1000); // Send data every second
+
+    ws.on('close', () => {
+        clearInterval(interval);
+        console.log("WebSocket connection closed");
+    });
+});
+
+// MongoDB-related routes remain unchanged
+app.get("/getAverage", function (req, res) {
+    var from = parseInt(req.query.from);
+    var to = parseInt(req.query.to);
+    db.collection("dataWeather").find({ time: { $gt: from, $lt: to } }).toArray(function (err, result) {
+        console.log(err);
+        console.log(result);
+        var tempSum = 0;
+        var humSum = 0;
+        for (var i = 0; i < result.length; i++) {
+            tempSum += result[i].t || 0;
+            humSum += result[i].t || 0;
+        }
+        var tAvg = tempSum / result.length;
+        var hAvg = humSum / result.length;
+        res.send(tAvg + " " + hAvg);
+    });
+});
+
+app.get("/getValue", function (req, res) {
+    res.send(VALUEt.toString() + " " + VALUEh + " " + VALUEtime + "\r");
+});
+
+app.get("/setValue", function (req, res) {
+    VALUEt = parseFloat(req.query.t);
+    VALUEh = parseFloat(req.query.h);
+    VALUEtime = new Date().getTime();
+    var dataObj = {
+        t: VALUEt,
+        h: VALUEh,
+        time: VALUEtime
+    };
+    res.send(VALUEtime.toString());
+    (async function () {
+        let client = await MongoClient.connect(connectionString, { useNewUrlParser: true });
+        let db = client.db('sensorData');
+        try {
+            let result = await db.collection("data").insertOne(dataObj);
+            if (result.insertedId) {
+                console.log(result.insertedId.toString());
+            }
+        } finally {
+            client.close();
+        }
+    })().catch(err => console.error(err));
+});
